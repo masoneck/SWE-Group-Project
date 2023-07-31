56ea2d8c8c37d4869c2d70f66ad7b5a75ef66da4
Lore <30599612+Lorenyx@users.noreply.github.com>
2023-07-31 02:24:51 -0500

    Add _select_query

--- src/model/database.py
+++ src/model/database.py
@@ -6,9 +6,21 @@ class Database:
         conn = sqlite3.connect(db_file_name)
         self._cursor = conn.cursor()
 
+    def _select_query(self, table: str, query: dict, is_cursor=False):
+        rows = self._cursor.execute(f"""
+        SELECT * FROM {table.capitalize()} WHERE {', '.join([f'{k}={v}' for k,v in query.items()])}
+        """)
+        return rows if is_cursor else rows.fetchall()
+
     def select_user(self, query: dict):
         """Select a user from the database"""
-        self._cursor.execute(f"""
-        SELECT * FROM Users WHERE {', '.join([f'{k}={v}' for k,v in query.items()])}
-        """)
+        return self._select_query('Users', query)
 
+
+    def select_sales_item(self, query: dict):
+        """Select a sales item from the database"""
+        return self._select_query('Items', query)
+    
+    def close(self):
+        """Close connection to database"""
+        return self._cursor.connection.close() and self._cursor.close()
