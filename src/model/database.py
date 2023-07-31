69daa6ef334a7703894fada2962446cf407ca06d
Lore <30599612+Lorenyx@users.noreply.github.com>
2023-07-31 03:01:56 -0500

    Add add_user statement

--- src/model/database.py
+++ src/model/database.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from src.model.user_model import UserModel
+
 class Database:
     """Class to execute queries and statements."""
 
@@ -9,7 +11,7 @@ class Database:
 
     def _select_query(self, table: str, query: dict, is_cursor=False):
         rows = self._cursor.execute(f"""
-        SELECT * FROM {table.capitalize()} WHERE {', '.join([f'{k}={v}' for k,v in query.items()])}
+        SELECT * FROM {table.capitalize()} WHERE {', '.join([f'{k}={v!r}' for k,v in query.items()])}
         """)
         return rows if is_cursor else rows.fetchall()
 
@@ -25,6 +27,18 @@ class Database:
     def select_order(self, query: dict):
         """Select an order from the database"""
         return self._select_query('Orders', query)
+
+    def _insert_statement(self, table: str, values: list, is_cursor=False):
+        """Insert values into database table. Assumes values are in correct order"""
+        rows = self._cursor.execute(f"""
+        INSERT INTO {table.capitalize()} VALUES ({', '.join(values)})
+        )""")
+        return rows if is_cursor else rows.fetchall()
+
+    def add_user(self, query: dict):
+        """Add a user to database"""
+        user = UserModel(**query)
+        return self._insert_statement('Users', user.to_list())
 
     def close(self):
         """Close connection to database"""
