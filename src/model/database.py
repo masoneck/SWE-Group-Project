18cb8a4d520e2d6f558d35ac1869517e26270d56
Lore <30599612+Lorenyx@users.noreply.github.com>
2023-07-31 02:29:51 -0500

    Add select_order

--- src/model/database.py
+++ src/model/database.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 class Database:
+    """Class to execute queries and statements."""
 
     def __init__(self, db_file_name):
         conn = sqlite3.connect(db_file_name)
@@ -20,7 +21,11 @@ class Database:
     def select_sales_item(self, query: dict):
         """Select a sales item from the database"""
         return self._select_query('Items', query)
-    
+
+    def select_order(self, query: dict):
+        """Select an order from the database"""
+        return self._select_query('Orders', query)
+
     def close(self):
         """Close connection to database"""
         return self._cursor.connection.close() and self._cursor.close()
