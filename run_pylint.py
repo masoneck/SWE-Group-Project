0c61fad99d589f663c090a3aa4bc0a861f9141c0
Mason <78109273+masoneck@users.noreply.github.com>
2023-08-02 22:39:10 -0500

    Update run_pylint

--- run_pylint.py
+++ run_pylint.py
@@ -1,6 +1,5 @@
 import subprocess
 
-
-
 if __name__ == '__main__':
-    subprocess.run(r'pylint .\tests\ .\src\ --disable=missing-module-docstring,import-error', check=True)
+    subprocess.run(r'pylint .\tests\ .\src\ --disable=missing-module-docstring,import-error,'
+                   r'too-many-function-args,too-many-arguments', check=True)
